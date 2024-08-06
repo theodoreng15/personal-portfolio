d9d895527bd90b2b335428440805b68548186d11
Theodore Ng <89175035+DayDreaming101@users.noreply.github.com>
2024-08-06 12:51:12 +0200

    Wrote About Me

--- src/app/components/Aboutme.tsx
+++ src/app/components/Aboutme.tsx
@@ -1,12 +1,19 @@
 const About = () => {
 	return (
 		<>
-			<div className="flex h-screen">
-				<div className="flex items-center text-4xl font-bold md:text-5xl ml-36 w-3/5 mb-36">
+			<div className="flex h-screen shadow">
+				<div className="flex items-center text-4xl font-bold md:text-5xl ml-36 pl-10 w-4/5 my-28 bg-gray-200">
 					About Me
 				</div>
-				<div className="flex items-center align-center text-xl md:text-2xl font-semibold p-8 mr-48">
-					Hi! I&apos;m Theodore Ng, a Computer Engineering student at the University of Illinois Urbana-Champaign. With a strong passion for Embedded Systems, Software Engineering, and Computer Architecture, I&apos;m excited about the future of technology and am eager to contribute to innovative projects. Feel free to connect or reach out with any questions! theodoreng15@gmail.com				</div>
+				<div className="flex-col  align-center text-xl md:text-2xl font-semibold p-8 mr-48  my-28  bg-gray-200">
+					<div>
+						Hi! I&apos;m Theodore Ng, a Computer Engineering student at the University of Illinois Urbana-Champaign. With a strong passion for Embedded Systems, Software Engineering, and Computer Architecture, I&apos;m really excited about the future of technology and am eager to contribute to any innovative projects. This upcoming year, I&apos;ll be taking: Computer Architecture, Applied Statistics, Analog Signal Processing, Data Structures, and Computer Systems & Programming.
+					</div>
+
+
+
+					In my own free time, I also enjoy watching Hikaru Nakumura, watching the NBA with friends, and listening to Pop/R&B music. Feel free to connect or reach out with any questions! theodoreng15@gmail.com 
+				</div>
 			</div>
 		</>);
 };
